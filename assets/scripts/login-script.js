9a9b4ec7cb626a25387e56a2b9aa93a04cba763f
milyasarmans <milyasarmans13@gmail.com>
2022-02-08 23:20:49 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -15,9 +15,15 @@ const expectedPassword = 'superpassword';
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
     // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
+
     /* TODO 2 : Buat Logika perbandingan dengan kondisi:
        Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
        Jika tidak, maka panggil fungsi showPopUp().
      */
+
+    if (email == expectedEmail && password == expectedPassword) {
+        goToHome();
+    } else {
+        showPopUp();
+    }
 });
